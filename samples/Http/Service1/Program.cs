3ce1274e04e90273a8e1b4ecc74ec0ebd60a0699
Li Jian <lj@eson101.com>
2019-10-28 17:59:58 +0800

    nuget update

---
+++ samples/Http/Service1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using DataContract;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using NetRpc.Http;
+
+namespace Service
+{
+    class Program
+    {
+        const string origins = "_myAllowSpecificOrigins";
+
+        static async Task Main(string[] args)
+        {
+            var webHost = WebHost.CreateDefaultBuilder(null)
+                .ConfigureServices(services =>
+                {
+                    services.AddCors(op =>
+                    {
+                        op.AddPolicy(origins, set =>
+                        {
+                            set.SetIsOriginAllowed(origin => true)
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowCredentials();
+                        });
+                    });
+
+                    services.AddSignalR();
+                    services.AddNetRpcSwagger();
+                    services.AddNetRpcHttpService();
+                    services.AddNetRpcContractSingleton<IServiceAsync, ServiceAsync>();
+                })
+                .Configure(app =>
+                {
+                    app.UseCors(origins);
+                    app.UseSignalR(routes => { routes.MapHub<CallbackHub>("/callback"); });
+                    app.UseNetRpcSwagger();
+                    app.UseNetRpcHttp();
+                })
+                .Build();
+
+            await webHost.RunAsync();
+        }
+    }
+}
--- samples/Http/Service1/Service.cs
+++ samples/Http/Service1/Service.cs
@@ -15,8 +15,7 @@ namespace Service
         {
             var retObj = new CustomObj {Date = DateTime.Now, Name = NameEnum.John};
             var h = GlobalActionExecutingContext.Context.Header;
-            //Console.WriteLine($"[Call]...receive:{p1}, {p2}, h1:{h["h1"]}, h2:{h["h2"]} return:{retObj}");
-            Console.WriteLine($"[Call]...receive:{p1}, {p2}, return:{retObj}");
+            Console.WriteLine($"[Call]...receive:{p1}, {p2}, h1:{h["h1"]}, h2:{h["h2"]} return:{retObj}");
             return retObj;
         }
 
