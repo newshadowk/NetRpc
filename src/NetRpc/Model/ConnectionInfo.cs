0d20e7f4a2f9f553e20d558ab979457ddb3e1150
Li Jian <lj@eson101.com>
2021-03-16 15:30:30 +0800

    optimize

--- src/NetRpc/Model/ConnectionInfo.cs
+++ src/NetRpc/Model/ConnectionInfo.cs
@@ -5,7 +5,7 @@ namespace NetRpc
     public sealed class ConnectionInfo
     {
         public ChannelType ChannelType { get; init; }
-        public string HeadHost { get; init; } = null!;
+        public string? HeadHost { get; init; }
         public string Host { get; init; } = null!;
         public int Port { get; init; }
         public string Description { get; init; } = null!;
