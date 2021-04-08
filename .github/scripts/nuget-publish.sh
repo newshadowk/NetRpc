605050d38ed703f77e33e2f0611cd41c9f65728e
杜克迪 <654858548@qq.com>
2021-04-08 17:05:52 +0800

    update nuget publish workflow

--- .github/scripts/nuget-publish.sh
+++ .github/scripts/nuget-publish.sh
@@ -3,15 +3,28 @@
 pwd=$1
 version=$2
 
-dotnet build all.sln --configuration Release
-dotnet nuget push  ".\src\NetRpc\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.Contract\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.Grpc\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.Http\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.Http.Client\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.MiniProfiler\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.Jaeger\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.OpenTracing\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\NetRpc.RabbitMQ\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\Proxy.Grpc\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
-dotnet nuget push  ".\src\Proxy.RabbitMQ\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet build All.sln --configuration Release
+
+dotnet pack ".\src\NetRpc" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.Contract" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.Grpc" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.Http" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.Http.Client" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.MiniProfiler" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.Jaeger" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.OpenTracing" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\NetRpc.RabbitMQ" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\Proxy.Grpc" -c Release -p:PackageVersion=$version
+dotnet pack ".\src\Proxy.RabbitMQ" -c Release -p:PackageVersion=$version
+
+dotnet nuget push ".\src\NetRpc\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.Contract\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.Grpc\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.Http\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.Http.Client\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.MiniProfiler\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.Jaeger\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.OpenTracing\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\NetRpc.RabbitMQ\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\Proxy.Grpc\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
+dotnet nuget push ".\src\Proxy.RabbitMQ\bin\Release\*.nupkg" -s http://nuget2.yx.com/nuget -k $pwd
