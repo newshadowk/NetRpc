56440dac878cfdc15d98ee248e8d3d4338da5cc1
杜克迪 <654858548@qq.com>
2021-04-12 16:02:35 +0800

    add ServiceNameAttribute

---
+++ src/NetRpc.Contract/Attribute/ServiceNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetRpc.Contract
+{
+    [AttributeUsage(AttributeTargets.Interface)]
+    public sealed class ServiceNameAttribute : Attribute
+    {
+        public ServiceNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
