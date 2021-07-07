13e23e31c121bbeb2381dec65ebba6be1d0016ce
Li Jian <lj@eson101.com>
2021-07-07 16:00:01 +0800

    hello world

--- samples/HelloWorld/Client/Program.cs
+++ samples/HelloWorld/Client/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using DataContract;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,41 +16,24 @@ namespace Client
     {
         private static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //register
+            var services = new ServiceCollection();
 
-            ////register
-            //var services = new ServiceCollection();
+            services.AddNGrpcClient(options =>
+            {
+                options.Url = "http://localhost:50001";
+                //options.HeaderHost = "www.baidu.com:8080";
+            });
+            services.AddNClientContract<IServiceAsync>();
+            var sp = services.BuildServiceProvider();
 
-            //services.AddNGrpcClient(options =>
-            //{
-            //    options.Url = "http://localhost:50001";
-            //    //options.HeaderHost = "www.baidu.com:8080";
-            //});
-            //services.AddNClientContract<IServiceAsync>();
-            //var sp = services.BuildServiceProvider();
+            //get service
+            var service = sp.GetService<IServiceAsync>();
+            Console.WriteLine("call: hello world.");
+            var ret = await service.CallAsync("hello world.");
+            Console.WriteLine($"ret: {ret}");
 
-            ////get service
-            //var service = sp.GetService<IServiceAsync>();
-            //Console.WriteLine("call: hello world.");
-            //var ret = await service.CallAsync("hello world.");
-            //Console.WriteLine($"ret: {ret}");
-
-            //Console.Read();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostContext, configApp) =>
-                {
-                    configApp.SetBasePath(Directory.GetCurrentDirectory());
-                    configApp.AddJsonFile(Path.Combine("config", "appsettings.json"),   true,  true);
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<LifetimeEventsHostedService>();
-                    services.Configure<COptions>(hostContext.Configuration.GetSection("C1"));
-                });
+            Console.Read();
         }
     }
 
