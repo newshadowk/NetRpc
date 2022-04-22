1ed55fcfd8cff242ac469b880719374919d76925
Li Jian <lj@eson101.com>
2022-04-22 10:53:00 +0800

    add busytimer

---
+++ src/NetRpc/Model/BusyTimer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace NetRpc;
+
+public sealed class BusyTimer : IDisposable
+{
+    private readonly Timer T = new();
+    private volatile bool _isStop;
+    private readonly object _lockStop = new();
+    public double Interval => T.Interval;
+
+    public BusyTimer(double interval)
+    {
+        T.Elapsed += T_Elapsed;
+        T.Interval = interval;
+    }
+
+    public void Dispose()
+    {
+        lock (_lockStop)
+        {
+            T.Dispose();
+            _isStop = true;
+        }
+    }
+
+    public void Start()
+    {
+        lock (_lockStop)
+        {
+            T.Start();
+            _isStop = false;
+        }
+    }
+
+    public void Stop()
+    {
+        lock (_lockStop)
+        {
+            T.Stop();
+            _isStop = true;
+        }
+    }
+
+    public event AsyncEventHandler<ElapsedEventArgs>? ElapsedAsync;
+
+    public event EventHandler<ElapsedEventArgs>? Elapsed;
+
+    private Task OnElapsedAsync(ElapsedEventArgs e)
+    {
+        return ElapsedAsync.InvokeAsync(this, e);
+    }
+
+    private async void T_Elapsed(object? sender, ElapsedEventArgs e)
+    {
+        T.Stop();
+ 
+        try
+        {
+            // ReSharper disable once MethodHasAsyncOverload
+            OnElapsed(e);
+            await OnElapsedAsync(e);
+        }
+        catch
+        {
+        }
+
+        lock (_lockStop)
+        {
+            if (!_isStop)
+                T.Start();
+        }
+    }
+
+    private void OnElapsed(ElapsedEventArgs e)
+    {
+        Elapsed?.Invoke(this, e);
+    }
+}
